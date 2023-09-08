6b9350ef8fdcd99c8a1ea6b71dbaa71470cbf1f4
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-09-08 11:41:56 +0900

    モデルセッターの追加

--- Game/GameScene/Title/Title.h
+++ Game/GameScene/Title/Title.h
@@ -16,6 +16,10 @@ public:
 
 	void Draw(Matrix4x4 viewProjection);
 	void Draw2D(Matrix4x4 viewProjection2d);
+public: // setter
+	void SetModels(std::vector<std::shared_ptr<Model>> modeldate) { mobModels_ = modeldate; };
+	void SetModelsType2(std::vector<std::shared_ptr<Model>> modeldate) { mobModels_type2 = modeldate; };
+	void SetBottonModels(std::vector<std::shared_ptr<Model>> modeldate) { bottonModels_ = modeldate; };
 
 private: // シーンで必要なモデル配列
 	//	プレイヤーデータのモデル配列
